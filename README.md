7bae329bc71f04abb337fd516e1014bd55d96c05
hvmonteiro <hugo.monteiro.pt@gmail.com>
2018-01-18 17:39:14 +0000

    Fixed colors when using with *watch* command. Updated README and added output example image.

--- README.md
+++ README.md
@@ -1,24 +1,50 @@
 # About
-BlessedCoins is a crypto currency monitor that runs in a terminal
+BlessedCoins is a crypto currency monitor that runs in a terminal showing the TOP10 CryptoCurrencies at the moment.
+
+
+# Description
+BlessedCoins is a crypto currency monitor that runs in a terminal using [CoinMarketapCap](http://www.coinmarketcap.com) REST API to get crypto currencies market data.
+It works behind a proxy if you have setup `https_proxy` or `http_proxy` environment variables.
+information display idea taken from [prettycoins](https://github.com/tiaanduplessis/prettycoins/)
+
+
+# Idea
+The idea is to have a simple yet powerfull way to get market information about crypto currencies without the assle of having to use a full-blown browser.
+
 
 # Usage
 Clone repository:
-`git clone https://github.com/hvmonteiro/blessedcoins.git`
+```
+git clone https://github.com/hvmonteiro/blessedcoins.git
+```
 
 Install NodeJS module dependencies:
-`npm install`
+```
+npm install
+```
 
-Execute it with default configurations which will monitor the TOP10 crypto currencies by default
-`npm start`
+Execute it to get a pretty table with all market information of the TOP10 Crypto Currencies
+```
+npm start
+```
 
-Notice: It works behind a proxy if you have setup *https_proxy* or *http_proxy* environment variables.
+## Output Example:
+![](images/output-example.png?raw=true)
 
-# Idea
-information display idea taken from [prettycoins](https://github.com/tiaanduplessis/prettycoins/)
+*Tip*: BlessedCoins runs best in a terminal with the dimensions 124x22 (or bigger).
+
+You can refresh the information every <n> seconds (this example is for 10 minutes) using `watch` unix command (remove `FORCE_COLOR=1` for black and white display):
+```
+watch --differences --interval 300 --beep --no-title --color  "FORCE_COLOR=1 npm --silent start"
+```
+*Notice*: data on [CoinMarketapCap](http://www.coinmarketcap.com) is only refreshed every 10 minutes, so logic dictates that you will unnecessary overload the API server if your refresh interval is less than 300 seconds (10 min).
+
 
 # License
 BlessedCoins is licensed under The MIT License (MIT)
 Check LICENSE file for more information.
 
+
 # Copyright
 Hugo Monteiro (c) 2018
+
--- index.js
+++ index.js
@@ -8,41 +8,45 @@ const CoinMarketCap = require("node-coinmarketcap");
 const DEFAULT_CURRENCY = "USD";
 
 const table = new Table({
-  chars: {
-    top: "",
-    "top-mid": "",
-    "top-left": "",
-    "top-right": "",
-    bottom: "",
-    "bottom-mid": "",
-    "bottom-left": "",
-    "bottom-right": "",
-    left: "",
-    "left-mid": "",
-    mid: "",
-    "mid-mid": "",
-    right: "",
-    "right-mid": "",
-    middle: ""
-  }
+    chars: {
+        top: "",
+        "top-mid": "",
+        "top-left": "",
+        "top-right": "",
+        bottom: "",
+        "bottom-mid": "",
+        "bottom-left": "",
+        "bottom-right": "",
+        left: "",
+        "left-mid": "",
+        mid: "",
+        "mid-mid": "",
+        right: "",
+        "right-mid": "",
+        middle: ""
+    }
 });
 
 
 const options = {
-  events: false,
-  refresh: 0,
-  convert: DEFAULT_CURRENCY
+    events: false,
+    refresh: 0,
+    convert: DEFAULT_CURRENCY
 };
 const client = new CoinMarketCap(options);
 
 const formatCurrency = val => {
-  return Number.parseFloat(val)
-    .toFixed(2)
-    .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    return Number.parseFloat(val)
+        .toFixed(2)
+        .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
 };
 
 const formatGrowth = val => {
-  return val.indexOf("-") === -1 ? chalk.green(val) : chalk.red(val);
+    return val.indexOf("-") === -1 ? chalk.green(val) : chalk.red(val);
+};
+
+const formatDefault = val => {
+    return chalk.white(val);
 };
 
 const readableNumber = val => {
@@ -54,61 +58,61 @@ const readableNumber = val => {
 }
 
 const formatRow = (data = {}) => {
-  let {
-    name,
-    symbol,
-    rank,
-    price_usd,
-    market_cap_usd,
-    "24h_volume_usd": volume_usd,
-    available_supply,
-    percent_change_1h,
-    percent_change_24h,
-    percent_change_7d
-  } = data;
+    let {
+        name,
+        symbol,
+        rank,
+        price_usd,
+        market_cap_usd,
+        "24h_volume_usd": volume_usd,
+        available_supply,
+        percent_change_1h,
+        percent_change_24h,
+        percent_change_7d
+    } = data;
 
-  percent_change_1h = formatGrowth(percent_change_1h);
-  percent_change_24h = formatGrowth(percent_change_24h);
-  percent_change_7d = formatGrowth(percent_change_7d);
+    percent_change_1h = formatGrowth(percent_change_1h);
+    percent_change_24h = formatGrowth(percent_change_24h);
+    percent_change_7d = formatGrowth(percent_change_7d);
 
-  (price_usd = Number.parseFloat(price_usd)),
-    (market_cap_usd = Number.parseFloat(market_cap_usd)),
-    (available_supply = Number.parseFloat(available_supply)),
-    (volume_usd = Number.parseFloat(volume_usd));
+    price_usd = Number.parseFloat(price_usd),
+    market_cap_usd = Number.parseFloat(market_cap_usd),
+    available_supply = Number.parseFloat(available_supply),
+    volume_usd = Number.parseFloat(volume_usd);
 
-  return [
-    rank,
-    name,
-    symbol,
-    percent_change_1h,
-    percent_change_24h,
-    percent_change_7d,
-    formatCurrency(price_usd),
-    readableNumber(market_cap_usd),
-    readableNumber(available_supply),
-    readableNumber(volume_usd)
-  ];
+    return [
+        formatDefault(rank),
+        formatDefault(name),
+        formatDefault(symbol),
+        percent_change_1h,
+        percent_change_24h,
+        percent_change_7d,
+        formatDefault(formatCurrency(price_usd)),
+        formatDefault(readableNumber(market_cap_usd)),
+        formatDefault(readableNumber(available_supply)),
+        formatDefault(readableNumber(volume_usd))
+    ];
 };
 
 const run = () => {
-  client.getTop(10, data => {
-    table.push(
-      [
-        "#",
-        "Name",
-        "Symbol",
-        "% 1h",
-        "% 24h",
-        "% 7d",
-        `Price (${options.convert})`,
-        `Market Cap (${options.convert})`,
-        `Circulating Supply (${options.convert})`,
-        `Volume (24h/${options.convert})`
-      ].map(val => chalk.bold(val))
-    );
-    data.map(formatRow).forEach(row => table.push(row));
-    console.log(table.toString());
-  });
+    client.getTop(10, data => {
+        table.push(
+            [
+                "#",
+                "Name",
+                "Symbol",
+                "% 1h",
+                "% 24h",
+                "% 7d",
+                `Price (${options.convert})`,
+                `Market Cap (${options.convert})`,
+                `Circulating Supply (${options.convert})`,
+                `Volume (24h/${options.convert})`
+            ].map(val => chalk.bold(val))
+        );
+        data.map(formatRow).forEach(row => table.push(row));
+        console.log(table.toString());
+    });
 };
 
 console.log("Getting TOP10 crypto currencies information from CoinMarketCap...");
