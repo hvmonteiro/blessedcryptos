32023f578ffda43006d1547fe0e1581e4f7939af
hvmonteiro <hugo@hugovmonteiro.net>
2018-01-25 19:46:57 +0000

    Added main menu and Orders box.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const programParams = require("commander");
 const formatCurrency = require("format-currency");
 const CoinMarketCap = require("node-coinmarketcap");
 const blessed = require("blessed");
-const program = blessed.program();
+const configJSON = require("json-fs-store")(".config.json");
 
 
 const TITLE = "Blessed Cryptos"
@@ -15,6 +15,14 @@ const MIN_SCREEN_HEIGHT = 40;
 
 const DEFAULT_CURRENCY = "USD";
 const TOP_CURRENCIES_NUMBER = 20;
+
+const configSettings = {
+    "tickers" : [
+        "BTC", 
+        "ETH", 
+        "LTC"],
+    "refresh" : "300"
+}
 
 const supportedCurrencies = [
     "AUD", "BRL", "CAD", "CHF", 
@@ -91,63 +99,11 @@ const formatTableRow = (data = {}) => {
     ];
 };
 
-//function list(val) {
-const list = (val = {}) => {
-    return val.split(",");
-}
-
-
-var tickersRawData = [];
-
-programParams
-    .version(VERSION)
-    .option("-c, --currency <value>", "Get market information about the symbol (ex: BTC, ETH, etc.).", function (value) { return value.split(",")} )
-    .option("-b, --base <value>", "Get market price against the specified base currency symbol. (Default: USD)", "USD")
-//    .option("-p, --price <n>", "Alert when the market matches the specified price.", parseFloat)
-//    .option("-P, --percentage <n>", "Alert when the market matches the specified percentage. (Default: 5)", parseFloat, "5")
-    .option("-t, --top [n] ", "Get specified top currencies (Default: " + TOP_CURRENCIES_NUMBER + ")", parseInt, TOP_CURRENCIES_NUMBER)
-//    .option("-a, --all ", "Get all existing currencies on the market", parseInt, 10)
-    .option("-R, --rawstats", "Displays symbol information as raw data (JSON format) instead of human readable. (Default: false)")
-    .option("-r, --refresh [n]", "Refresh information every <n> seconds. Run once and exit if not specified. (Default: 300)", 300, parseInt)
-    .parse(process.argv);
-/*
-if (!process.argv.slice(2).length) {
-    programParams.outputHelp();
-    process.exit(1);
-}
-*/
-if (programParams.args != "") {
-    console.log("\n Error: unknown command %s", programParams.args);
-    process.exit(1);
-}
-
-var cryptosList = programParams.currency; // Converted into an array by "list" function
-
-if (!programParams.top) { 
-    if (cryptosList == undefined) {
-        console.log("Error: Mandatory parameter 'currency' is missing."); 
-        process.exit(1);
-    }
-}
-if (supportedCurrencies.indexOf(programParams.base) === -1) {
-    console.log("Error: Specified convert currency not supported: %s", programParams.base); 
-    process.exit(1);
-}
-if (Number.isNaN(programParams.refresh))  {
-    console.log("Error: Incorrect refresh number specified: %s", programParams.refresh); 
-    process.exit(1);
-}
-    
-coinMarketCapOptions.refresh = (Number(coinMarketCapOptions.refresh) < 300) ? programParams.refresh : 300;
-
-var topNumber = programParams.top;
-
-const coinMarketCap = new CoinMarketCap(coinMarketCapOptions);
-
 // Create a screen object.
 const screen = blessed.screen({
     smartCSR: true,
     terminal: "xterm-256color",
+    ignoreLock: true
 });
 
 const runTop = () => {
@@ -181,20 +137,37 @@ const runTop = () => {
     });
 };
 
-screen.title = TITLE + "(" + VERSION + ")";
+// Create a list table to display ticker information
+const mainMenu = blessed.listbar({
+    parent: screen,
+    top: 0,
+    autoCommandKeys: true,
+    items: {
+        "Account" : { 
+            "Login" : "Logout" 
+        },
+        "Currency"  : "Currency",
+        "Exchanges" : {
+            0: "Bitfinex",
+            1: "Bittrex"
+        },
+        "Top 10" : "Top 10",
+        "Help" : "Help"
+    },
+});
 
-
-// Create a list box
+// Create a list table to display ticker information
 const tickerListTable = blessed.ListTable({
     label: " {bold}T{/}icker List ",
     parent: screen,
-    top: 0,
+    top: 1,
     left: 0,
     width: "100%",
     height: 18,
     tags: true,
     keys: true,
     vi: true,
+    draggable: true,
     noCellBorders: false,
     //    terminal: "xterm-256color",
     border: {
@@ -244,12 +217,13 @@ const tickerListTable = blessed.ListTable({
 
 const tickerDetailBox = blessed.box({
     parent: screen,
-    top: 18,
+    top: tickerListTable.top + tickerListTable.height,
+    bottom: 11,
     left: "0",
     width: "50%",
-    height: "50%",
     tags: true,
     keys: false,
+    draggable: true,
     border: {
         type: "line"
     },
@@ -266,12 +240,13 @@ const tickerDetailBox = blessed.box({
 
 const loggerMessagesBox = blessed.list({
     parent: screen,
-    top: 18,
+    top: tickerListTable.top + tickerListTable.height,
+    bottom: 11,
     right: "0",
     width: "50%",
-    height: "50%",
     tags: true,
     keys: false,
+    draggable: true,
     border: {
         type: "line"
     },
@@ -286,6 +261,29 @@ const loggerMessagesBox = blessed.list({
     content: "{center}ticker info{/}",
 });
 
+const ordersInfoBox = blessed.list({
+    parent: screen,
+    bottom: 1,
+    left: 0,
+    width: "100%",
+    height: 10,
+    tags: true,
+    keys: false,
+    draggable: true,
+    border: {
+        type: "line"
+    },
+    style: {
+        bg: "black",
+        border: {
+            fg: "gray"
+        }
+    },
+    label: " {bold}O{/}rders ",
+    content: "Selling order ...... ",
+});
+
+
 
 const promptBox = blessed.prompt({
     parent: screen,
@@ -299,6 +297,7 @@ const promptBox = blessed.prompt({
     border: "line",
     keys: true,
     vi: true,
+    draggable: true,
     mouse: true,
 });
 
@@ -354,6 +353,7 @@ const tickerInfoBox = blessed.box({
     width: 50,
     align: "center",
     tags: true,
+    draggable: true,
     hidden: true,
     border: "line",
     label: " Ticker Info ",
@@ -373,6 +373,61 @@ const messageBox = blessed.message({
   border: "line"
 });
 
+//function list(val) {
+const list = (val = {}) => {
+    return val.split(",");
+}
+
+
+var tickersRawData = [];
+
+programParams
+    .version(VERSION)
+    .option("-c, --currency <value>", "Get market information about the symbol (ex: BTC, ETH, etc.).", function (value) { return value.split(",")} )
+    .option("-b, --base <value>", "Get market price against the specified base currency symbol. (Default: USD)", "USD")
+//    .option("-p, --price <n>", "Alert when the market matches the specified price.", parseFloat)
+//    .option("-P, --percentage <n>", "Alert when the market matches the specified percentage. (Default: 5)", parseFloat, "5")
+    .option("-t, --top [n] ", "Get specified top currencies (Default: " + TOP_CURRENCIES_NUMBER + ")", parseInt, TOP_CURRENCIES_NUMBER)
+//    .option("-a, --all ", "Get all existing currencies on the market", parseInt, 10)
+    .option("-R, --rawstats", "Displays symbol information as raw data (JSON format) instead of human readable. (Default: false)")
+    .option("-r, --refresh [n]", "Refresh information every <n> seconds. Run once and exit if not specified. (Default: 300)", 300, parseInt)
+    .parse(process.argv);
+/*
+if (!process.argv.slice(2).length) {
+    programParams.outputHelp();
+    process.exit(1);
+}
+*/
+if (programParams.args != "") {
+    console.log("\n Error: unknown command %s", programParams.args);
+    process.exit(1);
+}
+
+var cryptosList = programParams.currency; // Converted into an array by "list" function
+
+if (!programParams.top) { 
+    if (cryptosList == undefined) {
+        console.log("Error: Mandatory parameter 'currency' is missing."); 
+        process.exit(1);
+    }
+}
+if (supportedCurrencies.indexOf(programParams.base) === -1) {
+    console.log("Error: Specified convert currency not supported: %s", programParams.base); 
+    process.exit(1);
+}
+if (Number.isNaN(programParams.refresh))  {
+    console.log("Error: Incorrect refresh number specified: %s", programParams.refresh); 
+    process.exit(1);
+}
+    
+coinMarketCapOptions.refresh = (Number(coinMarketCapOptions.refresh) < 300) ? programParams.refresh : 300;
+
+var topNumber = programParams.top;
+
+const coinMarketCap = new CoinMarketCap(coinMarketCapOptions);
+
+
+screen.title = TITLE + "(" + VERSION + ")";
 
 // get and setup tickers first time
 if (programParams.top) runTop();
@@ -391,8 +446,10 @@ tickerInfoBox.key(["escape", "enter"], function(ch, key) {
 });
 
 
+
 // Show when pressing Enter over an item
 tickerListTable.on("select", function(el, selected) {
+    mainMenu.select(0);
     if (tickerListTable._.rendering) return;
 
     // Get row as text, trim spaces, remove indent cell spaces and convert to an array
@@ -442,9 +499,9 @@ screen.key(["q", "C-c"], function(ch, key) {
 tickerListTable.focus();
 
 
-if (program.cols < MIN_SCREEN_WIDTH) {
+if (blessed.program.cols < MIN_SCREEN_WIDTH) {
     console.log("This program can only open on a terminal with a minimum of 140x40 characters.");
-    console.log("Your terminal is %sx%s.", program.cols, program.rows);
+    console.log("Your terminal is %sx%s.", blessed.program.cols, blessed.program.rows);
     process.exit();
 }
 
